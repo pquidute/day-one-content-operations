c51781618d202aa38c35d5d1e7f77f71044cff4f
Pedro Quidute <pquidute@proton.me>
2026-04-21 11:46:25 -0300

    feat: add `media` plugin media browsing

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,6 +4,7 @@ import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 import {structure} from './structure'
 import {defaultDocumentNode} from './structure/defaultDocumentNode'
+import {media} from 'sanity-plugin-media'
 
 export default defineConfig({
   name: 'default',
@@ -11,7 +12,7 @@ export default defineConfig({
 
   projectId: '1l1r6zzx',
   dataset: 'production',
-  plugins: [structureTool({structure, defaultDocumentNode}), visionTool()],
+  plugins: [structureTool({structure, defaultDocumentNode}), visionTool(), media()],
 
   schema: {
     types: schemaTypes,
@@ -24,5 +25,5 @@ export default defineConfig({
     }
 
     return prev.filter((tool) => tool.name !== 'vision')
-  }
+  },
 })
