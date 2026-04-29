4ebdeb766d4481768fcbbdb16ee1f3f9688ab7f8
Pedro Quidute <pquidute@proton.me>
2026-04-29 20:44:34 -0300

    chore: configure `app sdk`

--- apps/tickets/sanity.cli.ts
+++ apps/tickets/sanity.cli.ts
@@ -1,8 +1,14 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
   app: {
-    organizationId: 'oz0YEAkuF',
-    entry: './src/App.tsx',
+    organizationId: "oz0YEAkuF",
+    entry: "./src/App.tsx",
   },
-})
+  deployment: {
+    appId: "rlnmrz3scra1wu6ccvud9bd7",
+  },
+  server: {
+    port: 3334, // it runs by default on port 3333 (currently occupied by studio application), so we move it manually to another port
+  },
+});
--- apps/web/src/sanity/types.ts
+++ apps/web/src/sanity/types.ts
@@ -233,25 +233,7 @@ export type Geopoint = {
   alt?: number;
 };
 
-export type AllSanitySchemaTypes =
-  | VenueReference
-  | ArtistReference
-  | SanityImageAssetReference
-  | Event
-  | SanityImageCrop
-  | SanityImageHotspot
-  | Artist
-  | Venue
-  | Slug
-  | MediaTag
-  | SanityImagePaletteSwatch
-  | SanityImagePalette
-  | SanityImageDimensions
-  | SanityImageMetadata
-  | SanityFileAsset
-  | SanityAssetSourceData
-  | SanityImageAsset
-  | Geopoint;
+export type AllSanitySchemaTypes = VenueReference | ArtistReference | SanityImageAssetReference | Event | SanityImageCrop | SanityImageHotspot | Artist | Venue | Slug | MediaTag | SanityImagePaletteSwatch | SanityImagePalette | SanityImageDimensions | SanityImageMetadata | SanityFileAsset | SanityAssetSourceData | SanityImageAsset | Geopoint;
 
 // Source: ../web/src/app/events/[slug]/page.tsx
 // Variable: EVENT_QUERY
@@ -323,7 +305,7 @@ export type EVENT_QUERY_RESULT = {
 
 // Source: ../web/src/app/page.tsx
 // Variable: EVENTS_QUERY
-// Query: *[  _type == "event"  && defined(slug.current)  && date > now()]|order(date asc){_id, name, slug, date}
+// Query: *[  _type == "event"  && defined(slug.current)]|order(date asc){_id, name, slug, date}
 export type EVENTS_QUERY_RESULT = Array<{
   _id: string;
   name: string | null;
@@ -331,11 +313,3 @@ export type EVENTS_QUERY_RESULT = Array<{
   date: string | null;
 }>;
 
-// Query TypeMap
-import "@sanity/client";
-declare module "@sanity/client" {
-  interface SanityQueries {
-    '*[\n    _type == "event" &&\n    slug.current == $slug\n  ][0]{\n  ...,\n  "date": coalesce(date, now()),\n  "doorsOpen": coalesce(doorsOpen, 0),\n  headline->,\n  venue->\n}': EVENT_QUERY_RESULT;
-    '*[\n  _type == "event"\n  && defined(slug.current)\n  && date > now()\n]|order(date asc){_id, name, slug, date}': EVENTS_QUERY_RESULT;
-  }
-}
