a3d6e1e74722223554829c73bbc4a80c797315fd
Pedro Quidute <pquidute@proton.me>
2026-07-02 13:13:00 -0300

    feat: implement `first-published` function to run on document `creations` and `updates`;

--- functions/first-published/index.ts
+++ functions/first-published/index.ts
@@ -1,6 +1,23 @@
-import { documentEventHandler } from '@sanity/functions'
+import { createClient } from "@sanity/client";
+import { documentEventHandler } from "@sanity/functions";
 
 export const handler = documentEventHandler(async ({ context, event }) => {
-  const time = new Date().toLocaleTimeString()
-  console.log(`👋 Your Sanity Function was called at ${time}`)
-})
+  try {
+    await createClient({
+      ...context.clientOptions,
+      useCdn: false,
+      apiVersion: "2026-02-27",
+    })
+      .patch(event.data._id)
+      .setIfMissing({
+        firstPublished: new Date().toISOString(),
+      })
+      .commit({ dryRun: context.local });
+    console.log(
+      context.local ? "Dry run:" : "Updated:",
+      `firstPublished set on ${event.data._id}`,
+    );
+  } catch (error) {
+    console.error(error);
+  }
+});
